aceec7d542dadf8cd0f3b91efc15a760c8f92592
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-10-31 17:49:46 +0000

    Add codeaction that logs a message

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,12 +1,13 @@
-import * as path from 'path'
-import * as Mocha from 'mocha'
-import * as glob from 'glob'
+import path from 'path'
+import Mocha from 'mocha'
+import glob from 'glob'
 
 export function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
     ui: 'tdd',
     color: true,
+    timeout: 20000,
   })
 
   const testsRoot = path.resolve(__dirname, '..')
