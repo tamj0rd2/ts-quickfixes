e040f79b8799c7807a9191096ba613b0ecd41081
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-11-03 21:33:08 +0000

    WIP only get missing member info

---
+++ CONTRIBUTING.md
@@ -0,0 +1,7 @@
+# Contributing
+
+## Troubleshooting
+
+If the extension fails to activate when using the `Run Extension` launch config,
+try completely exiting vscode, `cd`ing into this repo, then running `code`.
+It seems to get confused about where to find project files unless I do this.
--- src/member-parser.spec.ts
+++ src/member-parser.spec.ts
@@ -14,10 +14,15 @@ describe('MemberParser', () => {
       mobileNumber: { countryCode: MemberType.String, phoneNumber: 0 },
       status: MemberType.Union,
     })
+  })
+
+  it('returns the correct members when there is already one specified', () => {
+    const memberParser = new MemberParser()
+
+    const members = memberParser.getMissingMembersForVariable('personWithOneProperty')
 
     expect(members).toStrictEqual<typeof members>({
       firstName: MemberType.String,
-      lastName: MemberType.String,
       birthday: MemberType.BuiltIn,
       address: { city: MemberType.String, postcode: MemberType.String },
       mobileNumber: { countryCode: MemberType.String, phoneNumber: 0 },
