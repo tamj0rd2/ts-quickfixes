4688d0c9472454f65c42637766ece741303c9b8e
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-11-09 12:42:16 +0000

    chore: start switching to a monorepo style repository

---
+++ .eslintrc.js
@@ -0,0 +1,78 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    project: [
+      `${__dirname}/tsconfig.json`,
+      `${__dirname}/tsconfig.tools.json`,
+      `${__dirname}/apps/extension/tsconfig.json`,
+      `${__dirname}/apps/extension/test-environment/tsconfig.json`,
+    ],
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+  ],
+  rules: {
+    'no-unused-expressions': 'off',
+    'require-await': 'off',
+    'prefer-promise-reject-errors': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+        allowHigherOrderFunctions: true,
+        allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        tabWidth: 2,
+        useTabs: false,
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'all',
+        printWidth: 110,
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
+      },
+    ],
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+      },
+    ],
+    '@typescript-eslint/require-await': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.spec.ts'],
+      rules: {
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
+    {
+      files: ['**/*.test.ts'],
+      rules: {
+        'jest/expect-expect': 'off',
+      },
+    },
+  ],
+}
