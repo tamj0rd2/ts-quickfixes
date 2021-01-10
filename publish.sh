3a6ffa5c3ee8b533a864eaf1d448bad5af2aecc1
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2021-01-10 17:47:55 +0000

    ci: try to fix vsce publishing

--- publish.sh
+++ publish.sh
@@ -1,3 +1,2 @@
 yarn build
-VERSION=$(jq -r .version packages/extension/package.json)
-(cd ./packages/extension && npx vsce publish $VERSION)
+npx lerna run --scope ts-quickfixes-extension --stream vscePublish
