496665d3f71bfaf69eb65e45ff77cbceace551bc
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2021-01-10 17:23:07 +0000

    fix(extension): bump plugin version fixing ts incompatabilities

--- packages/extension/scripts/shipit.sh
+++ packages/extension/scripts/shipit.sh
@@ -21,7 +21,14 @@ function finish {
 }
 trap finish EXIT
 
+CURRENT_VERSION_NUMBER="$(node ./scripts/get-marketplace-version.js)"
 semantic-release
 print 'Semantic release complete'
+UPDATED_VERSION_NUMBER="$(node ./scripts/get-marketplace-version.js)"
 
-print 'Publishing to vsce is currently disabled. Do it manually' $RED
+if [ "$CURRENT_VERSION_NUMBER" == "$UPDATED_VERSION_NUMBER" ]; then
+  print 'Not publishing the extension because there was no version increment'
+else
+  print "Going to publish the extension under version ${UPDATED_VERSION_NUMBER}"
+  vsce publish -p $PUBLISHER_TOKEN $UPDATED_VERSION_NUMBER
+fi
